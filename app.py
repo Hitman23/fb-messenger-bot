6788dcb0e71766bda8041a1d50805c3d2152c1bf
Hitman23 <mugtrade@gmail.com>
2017-03-27 03:08:35 +0300

    else message and more

--- app.py
+++ app.py
@@ -38,16 +38,10 @@ def webhook():
                     sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
                     recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
                     message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "Hello how are you, today!")
-
-                else:
-                    sender_id = messaging_event["sender"]["id"]  # the facebook ID of the person sending you the message
-                    recipient_id = messaging_event["recipient"][
-                        "id"]  # the recipient's ID, which should be your page's facebook ID
-                    message_text = messaging_event["message"]["text"]  # the message's text
-
-                    send_message(sender_id, "Sorry, didn't quite get that!")
+                    if message_text == "hi" or message_text ==" hey" or message_text =="hallo":
+                        send_message(sender_id, "Hello how are you, today!")
+                    else:
+                        send_message(sender_id, "Sorry didn't quite get that!")
 
                 if messaging_event.get("delivery"):  # delivery confirmation
                     pass
